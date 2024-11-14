f33f7bd27b922848c677ac030d0b5b4597981e78
Floyd <zweydfg8+github@rub.de>
2024-11-14 16:05:08 +0100

    optimized gaus over f16

--- src/16/arith.c
+++ src/16/arith.c
@@ -1,5 +1,7 @@
 #include "arith.h"
 
+#include <string.h>
+
 #define TS 512
 
 uint32_t test_transpose() {
@@ -22,15 +24,77 @@ uint32_t test_transpose() {
         data1[i] = i%16;
     }
 
-    gf16_matrix_print(data1, 64, 64);
+    // gf16_matrix_print(data1, 64, 64);
     gf16_transpose_64x64_avx2(data2, data1, 32);
-    gf16_matrix_print(data2, 64, 64);
+    // gf16_matrix_print(data2, 64, 64);
     free(data1); free(data2);
 #endif
-    return 1;
+    return 0;
+}
+
+uint32_t test_transpose2() {
+    const size_t s = 64;
+    uint8_t *data1 = calloc(1, s*s/2);
+    uint8_t *data2 = calloc(1, s*s/2);
+    uint8_t *data3 = calloc(1, s*s/2);
+
+    for (size_t i = 0; i < s; i++) {
+        for (size_t j = 0; j < s; j++) {
+            gf16_matrix_set_entry(data1, s, i, j, (i+j)%16);
+        }
+    }
+
+    gf16_matrix_tranpose(data2, data1, s, s);
+    gf16_matrix_tranpose_opt(data3, data1, s, s);
+
+    // gf16_matrix_print(data1, s, s);
+    // gf16_matrix_print(data2, s, s);
+    // gf16_matrix_print(data3, s, s);
+
+    if (memcmp(data2, data3, s*s/2) != 0) {
+        printf("kek\n");
+        return 1;
+    }
+
+    free(data1); free(data2); free(data3);
+    return 0;
+}
+
+uint32_t test_solve() {
+    const size_t s = 78;
+    uint8_t *A = calloc(1, s*s/2);
+    uint8_t *b = calloc(1, s/2);
+    uint8_t *AT = calloc(1, s*s/2);
+
+    gf16_matrix_rng_full_rank(A, s, s);
+
+    gf16_matrix_print(A, s, s);
+    // gf16_solve(A, b, s, s);
+    const uint32_t srows = gf16_solve_transpose(A, AT, b, s, s);
+    gf16_matrix_print(A, s, s);
+
+    if (srows != s) {
+        printf("wrong rank\n");
+        return 1;
+    }
+
+    for (uint32_t i = 0; i < s; i++) {
+        for (uint32_t j = 0; j < s; j++) {
+             if (gf16_matrix_get_entry(A, s, i, j) != (i == j)) {
+                 printf("kek\n");
+                 return 1;
+             }
+        }
+    }
+
+    free(A); free(b); free(AT);
+    return 0;
 }
 
 int main() {
     if (test_transpose()) { return 1; }
+    if (test_transpose2()) { return 1; }
+    if (test_solve()) { return 1; }
+
     return 0;
 }
--- src/16/bench.cpp
+++ src/16/bench.cpp
@@ -1,15 +1,25 @@
 #include <benchmark/benchmark.h>
 #include "arith.h"
 
+static long long cpucycles(void) noexcept {
+    unsigned long long result;
+    asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
+                 : "=a"(result)::"%rdx");
+    return result;
+}
+
 static void BM_gf16v_mul(benchmark::State& state) {
-    uint64_t a = 1;
+    uint64_t a = 1, c = 0;
     uint8_t b = 2;
     for (auto _ : state) {
+        c -= cpucycles();
         a = gf16v_mul(a, b);
+        c += cpucycles();
         a += 1;
         b += 1;
         benchmark::DoNotOptimize(a);
     }
+    state.counters["cycles"] = (double)c/(double)state.iterations();
 }
 
 
@@ -17,8 +27,11 @@ static void BM_gf16_matrix_transpose_16x16(benchmark::State& state) {
     auto *data1 = (uint8_t *)calloc(1, 256);
     auto *data2 = (uint8_t *)calloc(1, 256);
     uint8_t a = 0;
+    uint64_t c = 0;
     for (auto _ : state) {
+        c -= cpucycles();
         gf16_transpose_16x16(data2, data1, 8);
+        c += cpucycles();
         a += data2[8];
         gf16_transpose_16x16(data1, data2, 8);
         a += data1[8];
@@ -26,6 +39,7 @@ static void BM_gf16_matrix_transpose_16x16(benchmark::State& state) {
         benchmark::DoNotOptimize(a+=1);
     }
 
+    state.counters["cycles"] = (double)c/(double)state.iterations();
     free(data1); free(data2);
 }
 
@@ -33,8 +47,11 @@ static void BM_gf16_matrix_transpose_64x64(benchmark::State& state) {
     auto *data1 = (uint8_t *)calloc(1, 2048);
     auto *data2 = (uint8_t *)calloc(1, 2048);
     uint8_t a = 0;
+    uint64_t c = 0;
     for (auto _ : state) {
+        c -= cpucycles();
         gf16_transpose_64x64(data2, data1, 0);
+        c += cpucycles();
         a += data2[8];
         gf16_transpose_64x64(data1, data2, 0);
         a += data1[8];
@@ -42,7 +59,28 @@ static void BM_gf16_matrix_transpose_64x64(benchmark::State& state) {
         benchmark::DoNotOptimize(a+=1);
     }
 
+    state.counters["cycles"] = (double)c/(double)state.iterations();
     free(data1); free(data2);
+}
+
+
+static void BM_gf16_solve(benchmark::State& state) {
+    auto *A = (uint8_t *)calloc(1, 2048*2);
+    auto *b = (uint8_t *)calloc(1,  128*2);
+
+    gf16_matrix_rng(A, 78, 78);
+    uint8_t a = 1;
+    uint64_t c = 0;
+    for (auto _ : state) {
+        c -= cpucycles();
+        a+= gf16_solve(A+(8*(a&3)), b, 78, 78);
+        c += cpucycles();
+        a += A[8];
+        benchmark::DoNotOptimize(a+=1);
+    }
+
+    state.counters["cycles"] = (double)c/(double)state.iterations();
+    free(A); free(b);
 }
 #ifdef USE_AVX2
 
@@ -50,47 +88,82 @@ static void BM_gf16v_mul_u128(benchmark::State& state) {
     const __m128i acc = {783246, 872346};
     __m128i a = {23874687,905389},
             b = {87326457823,234652734687};
+    uint64_t c = 0;
     for (auto _ : state) {
+        c -= cpucycles();
         a = gf16v_mul_u128(a, b);
+        c += cpucycles();
         a ^= acc;
         benchmark::DoNotOptimize(a);
     }
+
+    state.counters["cycles"] = (double)c/(double)state.iterations();
 }
 
 static void BM_gf16v_mul_u256(benchmark::State& state) {
     const __m256i acc = {783246, 872346, 876502, 236478234023948};
     __m256i a = {23874687,905389,235645274,873465876},
             b = {87326457823,234652734687,8723468726347,78234687236478};
+    uint64_t c = 0;
     for (auto _ : state) {
+        c -= cpucycles();
         a = gf16v_mul_u256(a, b);
+        c += cpucycles();
         a ^= acc;
         benchmark::DoNotOptimize(a);
     }
+
+    state.counters["cycles"] = (double)c/(double)state.iterations();
 }
 
 static void BM_gf16_matrix_transpose_64x64_avx2(benchmark::State& state) {
     auto *data1 = (uint8_t *)calloc(1, 2048);
     auto *data2 = (uint8_t *)calloc(1, 2048);
     uint8_t a = 0;
+    uint64_t c = 0;
     for (auto _ : state) {
         gf16_transpose_64x64_avx2(data2, data1, 32);
         a += data2[8];
+        c -= cpucycles();
         gf16_transpose_64x64_avx2(data1, data2, 32);
+        c += cpucycles();
         a += data1[8];
 
         benchmark::DoNotOptimize(a+=1);
     }
 
+    state.counters["cycles"] = (double)c/(double)state.iterations();
     free(data1); free(data2);
+}
+
+static void BM_gf16_solve_transpose(benchmark::State& state) {
+    auto *A = (uint8_t *)calloc(1, 2048*2);
+    auto *AT = (uint8_t *)calloc(1, 2048*2);
+    auto *b = (uint8_t *)calloc(1,  128*2);
+    gf16_matrix_rng(A, 78, 78);
+    uint8_t a = 1;
+    uint64_t c = 0;
+    for (auto _ : state) {
+        c -= cpucycles();
+        a+= gf16_solve_transpose(A+(8*(a&3)), AT, b, 78, 78);
+        c += cpucycles();
+        a += A[8];
+        benchmark::DoNotOptimize(a+=1);
+    }
+
+    state.counters["cycles"] = (double)c/(double)state.iterations();
+    free(A); free(b); free(AT);
 }
 
 BENCHMARK(BM_gf16v_mul_u128);
 BENCHMARK(BM_gf16v_mul_u256);
 BENCHMARK(BM_gf16_matrix_transpose_64x64_avx2);
+BENCHMARK(BM_gf16_solve_transpose);
 #endif
 
 BENCHMARK(BM_gf16v_mul);
 BENCHMARK(BM_gf16_matrix_transpose_16x16);
 BENCHMARK(BM_gf16_matrix_transpose_64x64);
+BENCHMARK(BM_gf16_solve);
 BENCHMARK_MAIN();
 
