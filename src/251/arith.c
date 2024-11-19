41897bbba9a0c6936f6575de406ecbb048984b4f
floyd <floyd.zweydinger@rub.de>
2024-11-19 12:57:43 +0100

    added gf31 avx

--- src/251/arith.c
+++ src/251/arith.c
@@ -1,3 +1,58 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "arith.h"
+
+#ifdef USE_AVX2
+uint32_t test_vector_add() {
+    uint8_t tmp[32];
+    for (uint32_t i = 0; i < PRIME; i++) {
+        for (uint32_t j = 8; j < PRIME; j++) {
+            const ff_t c = gf251_add(i, j);
+            const __m256i a = _mm256_set1_epi8(i);
+            const __m256i b = _mm256_set1_epi8(j);
+            const __m256i d = gf251v_add_u256(a, b);
+
+            _mm256_storeu_si256((__m256i *)tmp, d);
+            for (uint32_t k = 0; k < 32; k++){
+                if (tmp[k] != c) {
+                    printf("test_vector_add full\n");
+                    return 1;
+                }
+            }
+        }
+
+    }
+
+    return 0;
+}
+
+uint32_t test_vector_mul() {
+    uint8_t tmp[32];
+    for (uint32_t i = 0; i < PRIME; i++) {
+        for (uint32_t j = 0; j < PRIME; j++) {
+            const ff_t c = gf251_mul(i, j);
+            const __m256i a = _mm256_set1_epi8(i);
+            const __m256i b = _mm256_set1_epi8(j);
+            const __m256i d = gf251v_mul_u256(a, b);
+
+            _mm256_storeu_si256((__m256i *)tmp, d);
+            for (uint32_t k = 0; k < 32; k++){
+                if ((tmp[k]%31) != c) {
+                    printf("test_vector_mul\n");
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+#endif
+
 int main() {
+#ifdef USE_AVX2
+    if (test_vector_add()) { return 1; }
+    if (test_vector_mul()) { return 1; }
+#endif
     return 0;
 }
---
+++ src/31/arith.h
@@ -0,0 +1,112 @@
+#include <stdint.h>
+
+#define PRIME 31
+
+// Work as soon as x <= 63487 = 2^15.95
+#define _gf31_reduce16(x) ((x) - 31 * (((uint32_t)(x) * 67651) >> 21))
+
+// Work as soon as x <= 4160749567 = 2^31.95
+#define _gf31_reduce32(x) ((x) - 31 * (((uint64_t)(x) * 4433514629) >> 37))
+
+
+typedef uint8_t ff_t;
+
+ff_t gf31_add(ff_t a, ff_t b) {
+    return (a+b) % PRIME;
+}
+
+ff_t gf31_sub(ff_t a, ff_t b) {
+    return (a+PRIME-b) % PRIME;
+}
+
+ff_t gf31_mul(ff_t a, ff_t b) {
+    return (a*b)%PRIME;
+}
+
+ff_t gf31_neg(ff_t a) {
+    return (PRIME - a) % PRIME;
+}
+
+
+#ifdef USE_AVX2 
+#include <immintrin.h>
+
+/// NOTE: assumes that each gf31 element is in a single uint8_t
+/// NOTE: not really a full reduction, as 31 is not going to be reduced
+__m256i gf31v_red_u256(const __m256i a) {
+    const __m256i Q = _mm256_set1_epi8(31);
+    const __m256i mask = _mm256_set1_epi8(7u);
+    __m256i t = _mm256_srli_epi16(a, 5);
+    t &= mask;
+    t &= Q;
+    __m256i ret = _mm256_add_epi8(a, t);
+    ret &= Q;
+    return ret;
+}
+
+/// NOTE: assumes that each gf31 element is in a single uint8_t
+__m256i gf31v_full_red_u256(const __m256i a) {
+    const __m256i mod = _mm256_set1_epi8(31u);
+    const __m256i one = _mm256_set1_epi8(1u);
+    __m256i t;
+
+    t = _mm256_add_epi8(a, one);
+    t = _mm256_srli_epi16(t, 5);
+    t = _mm256_and_si256(t, one);
+    t = _mm256_add_epi8(a, t);
+    t = _mm256_and_si256(t, mod);
+    return t;
+}
+
+/// NOTE: assumes that each gf31 element is in a single uint8_t
+/// NOTE: not really a full reduction, as 31 is not going to be reduced
+__m256i gf31v_add_u256(const __m256i a,
+                       const __m256i b) {
+    const __m256i c = _mm256_add_epi8(a, b);
+    return gf31v_red_u256(c);
+}
+
+__m256i gf31v_full_add_u256(const __m256i a,
+                            const __m256i b) {
+    const __m256i c = _mm256_add_epi8(a, b);
+    return gf31v_full_red_u256(c);
+}
+
+/// NOTE: assumes reduced inputs
+/// NOTE: not full recution
+__m256i gf31v_mul_u256(const __m256i a,
+                       const __m256i b) {
+    __m256i tl, th, sl, sh, r;
+    const __m256i one = _mm256_set1_epi16(1);
+
+    const __m256i m = _mm256_set1_epi16(0xFF);
+    const __m256i al = a & m;
+    const __m256i ah = _mm256_srli_epi16(a, 8);
+    const __m256i bl = b & m;
+    const __m256i bh = _mm256_srli_epi16(b, 8);
+
+    // actual multiplication
+    const __m256i cl = _mm256_mullo_epi16(al, bl);
+    const __m256i ch = _mm256_mullo_epi16(ah, bh);
+
+    // reduction
+    tl = _mm256_srli_epi16(cl, 5);
+    th = _mm256_srli_epi16(ch, 5);
+    sl = _mm256_add_epi16(tl, cl);
+    sh = _mm256_add_epi16(th, ch);
+    tl = _mm256_slli_epi16(tl, 5);
+    th = _mm256_slli_epi16(th, 5);
+
+    r  = _mm256_sub_epi16(sl, tl);
+    sh = _mm256_sub_epi16(sh, th);
+    sh = _mm256_slli_epi16(sh, 8);
+    r = r ^ sh;
+    return r;
+}
+
+/// full
+__m256i gf31v_full_mul_u256(const __m256i a,
+                            const __m256i b) {
+    return gf31v_red_u256(gf31v_mul_u256(a, b));
+}
+#endif
