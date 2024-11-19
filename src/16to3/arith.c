8f9c3412af5ae5cb3aeabe4dedcb6f3c4f4aca55
floyd <floyd.zweydinger@rub.de>
2024-11-19 16:57:00 +0100

    added 16bit matrix arith

--- src/16to3/arith.c
+++ src/16to3/arith.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "arith.h"
 
@@ -49,6 +50,48 @@ uint32_t test_vector_mul() {
 
     return 0;
 }
+
+
+uint32_t test_matrix_mul() {
+    const uint32_t nrows = 8;
+    const uint32_t ncols = 8;
+    const uint32_t ncols2 = 8;
+    gf16to3 *m1 = gf16to3_matrix_alloc(nrows, ncols);
+    gf16to3 *m2 = gf16to3_matrix_alloc(nrows, ncols2);
+    gf16to3 *m3 = gf16to3_matrix_alloc(nrows, ncols2);
+    gf16to3 *m4 = gf16to3_matrix_alloc(nrows, ncols2);
+
+    gf16to3_matrix_id(m1, nrows, ncols);
+    gf16to3_matrix_id(m2, nrows, ncols);
+    gf16to3_matrix_mul(m3, m1, m2, nrows, ncols, ncols);
+    gf16to3_matrix_mul_8xX(m4, m1, m2, ncols, ncols2);
+
+    gf16to3_matrix_print(m3, ncols, ncols2);
+    gf16to3_matrix_print(m4, ncols, ncols2);
+
+    uint32_t ret = 0;
+    for (int i = 0; i < ncols; ++i) {
+        for (int j = 0; j < ncols2; ++j) {
+            gf16to3 c = gf16to3_matrix_get(m3, ncols, i, j);
+            gf16to3 d = gf16to3_matrix_get(m4, ncols, i, j);
+            if (c != (i == j)) {
+                printf("test matrix mul\n");
+                ret = 1;
+                goto finish;
+            }
+
+            if (d != (i == j)) {
+                printf("test matrix mul v2\n");
+                ret = 1;
+                goto finish;
+            }
+        }
+    }
+
+    finish:
+    free(m1); free(m2); free(m3); free(m4);
+    return ret;
+}
 #endif
 
 
@@ -57,7 +100,8 @@ int main() {
     if (test_add()) { return 1; }
     if (test_mul()) { return 1; }
 #ifdef USE_AVX2
-    if (test_vector_mul()) { return 1; }
+    // if (test_vector_mul()) { return 1; }
+    if (test_matrix_mul()) { return 1; }
 #endif
     return 0;
 }
